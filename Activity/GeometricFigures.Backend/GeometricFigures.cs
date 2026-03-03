dadb4979730827f4e3bed2a266ae4c4a7e40438a
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-03-02 23:05:43 -0500

    Next -Solution

---
+++ Activity/GeometricFigures.Backend/GeometricFigures.cs
@@ -0,0 +1,25 @@
+﻿namespace GeometricFigures.Backend;
+
+public abstract class GeometricFigures 
+{
+    public int Name       
+    {   
+        get;    
+        set;    
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+    public abstract double GetArea()
+    { 
+        
+    }
+
+    public abstract double GetPerimiter()
+    {
+
+    }
+
+}
---
+++ Activity/GeometricFigures.Backend/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace GeometricFigures.Backend;
+
+public class Square : GeometricFigures
+{
+    private int _a = 0;
+
+    public int A
+    {
+        get;
+        set;
+    }
+
+
+    public int Square()
+    {
+        
+
+    }
+
+    private int ValidateA() 
+    {
+        
+    }
+
+
+}
